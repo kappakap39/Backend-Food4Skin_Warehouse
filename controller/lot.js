3230190962644b8ece03309ba0eba7ba320e9d0f
Phuwadon <88771136+kappakap39@users.noreply.github.com>
2023-08-13 01:39:42 +0700

    update

--- controller/lot.js
+++ controller/lot.js
@@ -1,11 +1,24 @@
 import db from "../db.js";
 
+// export const selectlot = (req, res) => {
+
+//     const sql = "SELECT * FROM `lotproduct` JOIN product ON product.ID_product = lotproduct.ID_product ORDER BY `ID_lot` DESC"
+//     db.query(sql, (err, result)=>{
+//         if(err) return res.json({message: "error run result all Lot"});
+//         return res.json(result);
+
+//     })
+// }
+
+
 export const selectlot = (req, res) => {
-
     const sql = "SELECT * FROM `lotproduct` JOIN product ON product.ID_product = lotproduct.ID_product ORDER BY `ID_lot` DESC"
-    db.query(sql, (err, result)=>{
-        if(err) return res.json({message: "error run result all Lot"});
-        return res.json(result);
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ message: "error run result all Lot" });
 
-    })
+        // Convert the result to JSON string
+        const resultText = JSON.stringify(result);
+
+        return res.send(resultText);
+    });
 }
