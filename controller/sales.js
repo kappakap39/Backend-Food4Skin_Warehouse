db892f38d2088de5110f9ed06e611381fad581c0
Phuwadon <88771136+kappakap39@users.noreply.github.com>
2023-08-20 15:07:44 +0700

    update

--- controller/sales.js
+++ controller/sales.js
@@ -1,22 +1,73 @@
 import db from "../db.js";
 
-
 //select all sales
+// export const selectSales = (req, res) => {
+
+//     const sql = "SELECT * FROM `sales` ORDER BY `ID_sales` DESC"
+//     db.query(sql, (err, result)=>{
+//         if(err) return res.json({message: "error run result all"});
+//         return res.json(result);
+
+//     })
+// }
+
 export const selectSales = (req, res) => {
+    const sql = `
+        SELECT
+            ID_sales,
+            fullname,
+            email,
+            password,
+            sex,
+            CONCAT(
+                CASE
+                    WHEN LENGTH(Tel) = 9 THEN
+                        CONCAT(
+                            SUBSTRING(Tel, 1, 2), '-',
+                            SUBSTRING(Tel, 3, 3), '-',
+                            SUBSTRING(Tel, 6)
+                        )
+                    ELSE
+                        CONCAT(
+                            SUBSTRING(Tel, 1, 3), '-',
+                            SUBSTRING(Tel, 4, 3), '-',
+                            SUBSTRING(Tel, 7)
+                        )
+                END
+            ) AS PhoneNumber,
+            CONCAT(
+                SUBSTRING(IDcard, 1, 1), '-',
+                SUBSTRING(IDcard, 2, 4), '-',
+                SUBSTRING(IDcard, 6, 5), '-',
+                SUBSTRING(IDcard, 11, 2), '-',
+                SUBSTRING(IDcard, 13)
+            ) AS Card_ID,
+            province,
+            districts,
+            subdistricts,
+            AddressSale,
+            Persistent_status,
+            contact,
+            picture,
+            zip_code
+        FROM
+            sales
+        ORDER BY
+            ID_sales DESC`;
 
-    const sql = "SELECT * FROM `sales` ORDER BY `ID_sales` DESC"
-    db.query(sql, (err, result)=>{
-        if(err) return res.json({message: "error run result all"});
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.json({ message: "error running query" });
+        }
         return res.json(result);
-
-    })
-}
-
+    });
+};
 
 //add sales
 export const addsales = (req, res) => {
-    const sql = "INSERT INTO `sales`(`districts`, `ID_sales`, `fullname`, `email`, `password`, `sex`, `IDcard`, `province`, `subdistricts`, `AddressSale`, `Tel`, `Persistent_status`, `contact`, `picture`, `zip_code`) VALUES (?)";
-    const values =[
+    const sql =
+        "INSERT INTO `sales`(`districts`, `ID_sales`, `fullname`, `email`, `password`, `sex`, `IDcard`, `province`, `subdistricts`, `AddressSale`, `Tel`, `Persistent_status`, `contact`, `picture`, `zip_code`) VALUES (?)";
+    const values = [
         req.body.districts,
         req.body.ID_sales,
         req.body.fullname,
@@ -31,70 +82,141 @@ export const addsales = (req, res) => {
         req.body.Persistent_status,
         req.body.contact,
         req.body.picture,
-        req.body.zip_code
+        req.body.zip_code,
     ];
     db.query(sql, [values], (err, result) => {
-        if(err) return res.json(err);
-        return res.json(result);
-    });
-}
-
-//show one sales
-export const showdatasale =(req, res) => {
-
-    const sql = "SELECT * FROM sales WHERE ID_sales = ? ";
-    const ID_sales = req.params.ID_sales;
-    db.query(sql,[ID_sales], (err, dataRead)=>{
-        if(err) return res.json({Message: "Error inside server"});
-        return res.json(dataRead);
-    })
-
-}
-
-//update
-export const getupdateSale = (req, res) => {
-    
-    const sql = "SELECT * FROM `sales` WHERE `ID_sales` = ?";
-    const id = req.params.id;
-
-    db.query(sql, [id], (err, result) => {
-        if(err) return res.json({
-            Message:"Error inside server"
-        });
+        if (err) return res.json(err);
         return res.json(result);
     });
 };
 
-export const saleUpdate =(req, res) => {
-    const id =req.params.id;
-    const sql = "UPDATE `sales` SET `districts`=?,`fullname`=?,`email`=?,`password`=?,`sex`=?,`IDcard`=?,`province`=? ,`subdistricts`=? ,`AddressSale`=? ,`Tel`=? ,`Persistent_status`=?, `contact`=?, `picture`=?, `zip_code`=? WHERE `ID_sales` = ?";
+//show one sales
+export const showdatasale = (req, res) => {
+    const sql = `SELECT *,
+        CONCAT(
+            CASE
+                WHEN LENGTH(Tel) = 9 THEN
+                    CONCAT(
+                        SUBSTRING(Tel, 1, 2), '-',
+                        SUBSTRING(Tel, 3, 3), '-',
+                        SUBSTRING(Tel, 6)
+                    )
+                ELSE
+                    CONCAT(
+                        SUBSTRING(Tel, 1, 3), '-',
+                        SUBSTRING(Tel, 4, 3), '-',
+                        SUBSTRING(Tel, 7)
+                    )
+            END
+        ) AS PhoneNumber,
+        CONCAT(
+            SUBSTRING(IDcard, 1, 1), '-',
+            SUBSTRING(IDcard, 2, 4), '-',
+            SUBSTRING(IDcard, 6, 5), '-',
+            SUBSTRING(IDcard, 11, 2), '-',
+            SUBSTRING(IDcard, 13)
+        ) AS Card_ID
+    FROM sales
+    WHERE ID_sales = ?`;
+    
+    const ID_sales = req.params.ID_sales;
+    
+    db.query(sql, [ID_sales], (err, dataRead) => {
+        if (err) {
+            return res.json({ Message: "Error inside server" });
+        }
+        return res.json(dataRead);
+    });
+};
+
+
+//update
+// export const getupdateSale = (req, res) => {
+//     const sql = "SELECT * FROM `sales` WHERE `ID_sales` = ?";
+//     const id = req.params.id;
+
+//     db.query(sql, [id], (err, result) => {
+//         if (err)
+//             return res.json({
+//                 Message: "Error inside server",
+//             });
+//         return res.json(result);
+//     });
+// };
+
+export const getupdateSale = (req, res) => {
+    const sql = `SELECT *,
+        CONCAT(
+            CASE
+                WHEN LENGTH(Tel) = 9 THEN
+                    CONCAT(
+                        SUBSTRING(Tel, 1, 2), '-',
+                        SUBSTRING(Tel, 3, 3), '-',
+                        SUBSTRING(Tel, 6)
+                    )
+                ELSE
+                    CONCAT(
+                        SUBSTRING(Tel, 1, 3), '-',
+                        SUBSTRING(Tel, 4, 3), '-',
+                        SUBSTRING(Tel, 7)
+                    )
+            END
+        ) AS PhoneNumber,
+        CONCAT(
+            SUBSTRING(IDcard, 1, 1), '-',
+            SUBSTRING(IDcard, 2, 4), '-',
+            SUBSTRING(IDcard, 6, 5), '-',
+            SUBSTRING(IDcard, 11, 2), '-',
+            SUBSTRING(IDcard, 13)
+        ) AS Card_ID
+    FROM sales
+    WHERE ID_sales = ?`;
+
+    const id = req.params.id;
+
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.json({
+                Message: "Error inside server",
+            });
+        }
+        return res.json(result);
+    });
+};
+
+
+export const saleUpdate = (req, res) => {
+    const id = req.params.id;
+    const sql =
+        "UPDATE `sales` SET `districts`=?,`fullname`=?,`email`=?,`password`=?,`sex`=?,`IDcard`=?,`province`=? ,`subdistricts`=? ,`AddressSale`=? ,`Tel`=? ,`Persistent_status`=?, `contact`=?, `picture`=?, `zip_code`=? WHERE `ID_sales` = ?";
 
     db.query(
-        sql,[
-        req.body.districts,
-        req.body.fullname,
-        req.body.email,
-        req.body.password,
-        req.body.sex,
-        req.body.IDcard,
-        req.body.province,
-        req.body.subdistricts,
-        req.body.AddressSale,
-        req.body.Tel,
-        req.body.Persistent_status,
-        req.body.contact,
-        req.body.picture,
-        req.body.zip_code,
-        id,
+        sql,
+        [
+            req.body.districts,
+            req.body.fullname,
+            req.body.email,
+            req.body.password,
+            req.body.sex,
+            req.body.IDcard,
+            req.body.province,
+            req.body.subdistricts,
+            req.body.AddressSale,
+            req.body.Tel,
+            req.body.Persistent_status,
+            req.body.contact,
+            req.body.picture,
+            req.body.zip_code,
+            id,
         ],
 
         (err, result) => {
             if (err) {
-                return res.json({Message:" Error inside server"});
+                return res.json({ Message: " Error inside server" });
             }
             return res.json(result);
         }
-    )
+    );
 };
 
 //delete
@@ -102,26 +224,20 @@ export const deleteSales = (req, res) => {
     const sql = "DELETE FROM `sales` WHERE `ID_sales` = ?";
     const ID_sales = req.params.ID_sales;
     db.query(sql, [ID_sales], (err, result) => {
-        if(err) return res.json({Message: "Error inside server"});
+        if (err) return res.json({ Message: "Error inside server" });
         return res.json(result);
-    })
-}
+    });
+};
 
 //login Sales
 export const loginsales = (req, res) => {
     const sql = "SELECT * FROM `sales` WHERE `email`=? AND `password`=? ";
-    db.query(sql, [req.body.email, req.body.password ], (err, data) => {
-        if(err) return res.json("Error login");
-        if(data.length > 0){
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if (err) return res.json("Error login");
+        if (data.length > 0) {
             return res.json("Success");
         } else {
             return res.json("No record");
         }
-        
-    })
-}
-
-
-
-
-
+    });
+};
